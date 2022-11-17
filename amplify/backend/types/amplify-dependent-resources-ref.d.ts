e6f16be64ebe3caa9faee9011aef4fed48677954
Suraj Kalgudi <surajkalgudi@gmail.com>
2022-11-17 18:40:52 +0530

    Files Added

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "amplifiedtodo": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "amplifiedtodo": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
---
+++ lib/main.dart
@@ -0,0 +1,304 @@
+// dart async library you will refer to when setting up real time updates
+import 'dart:async';
+
+// flutter and ui libraries
+import 'package:flutter/material.dart';
+
+// amplify packages you will need to use
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:amplify_datastore/amplify_datastore.dart';
+import 'package:amplify_api/amplify_api.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+
+// amplify configuration and models that should have been generated for you
+import 'amplifyconfiguration.dart';
+import 'models/ModelProvider.dart';
+import 'models/Todo.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Amplified Todo',
+      home: TodosPage(),
+    );
+  }
+}
+
+class TodosPage extends StatefulWidget {
+  const TodosPage({Key? key}) : super(key: key);
+
+  @override
+  State<TodosPage> createState() => _TodosPageState();
+}
+
+class _TodosPageState extends State<TodosPage> {
+
+  late StreamSubscription<QuerySnapshot<Todo>> _subscription;
+  bool _isLoading=true;
+
+  List<Todo> _todos=[];
+
+
+  @override
+  void initState() {
+    // to be filled in a later step
+    _initializeApp();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _subscription.cancel();
+  }
+
+  Future<void> _initializeApp() async {
+    // configure Amplify
+    await _configureAmplify();
+
+    // Query and Observe updates to Todo models. DataStore.observeQuery() will
+    // emit an initial QuerySnapshot with a list of Todo models in the local store,
+    // and will emit subsequent snapshots as updates are made
+    //
+    // each time a snapshot is received, the following will happen:
+    // _isLoading is set to false if it is not already false
+    // _todos is set to the value in the latest snapshot
+    _subscription = Amplify.DataStore.observeQuery(Todo.classType)
+        .listen((QuerySnapshot<Todo> snapshot) {
+      setState(() {
+        if (_isLoading) _isLoading = false;
+        _todos = snapshot.items;
+      });
+    });
+  }
+
+  Future<void> _configureAmplify() async {
+    try {
+
+      // amplify plugins
+      final dataStorePlugin = AmplifyDataStore(modelProvider: ModelProvider.instance);
+      final apiPlugin = AmplifyAPI();
+      final authPlugin = AmplifyAuthCognito();
+      // add Amplify plugins
+      await Amplify.addPlugins([dataStorePlugin, apiPlugin, authPlugin]);
+
+      // configure Amplify
+      //
+      // note that Amplify cannot be configured more than once!
+      await Amplify.configure(amplifyconfig);
+    } catch (e) {
+
+      // error handling can be improved for sure!
+      // but this will be sufficient for the purposes of this tutorial
+      safePrint('An error occurred while configuring Amplify: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Todo List'),
+      ),
+      // body: const Center(child: CircularProgressIndicator()),
+       body: _isLoading
+           ? Center(child: CircularProgressIndicator())
+           : TodosList(todos: _todos),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const AddTodoForm()),
+          );
+        },
+        tooltip: 'Add Todo',
+        label: Row(
+          children: const [Icon(Icons.add), Text('Add todo')],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
+
+class TodosList extends StatelessWidget {
+  const TodosList({
+    required this.todos,
+    Key? key,
+  }) : super(key: key);
+
+  final List<Todo> todos;
+
+  @override
+  Widget build(BuildContext context) {
+    return todos.isNotEmpty
+        ? ListView(
+        padding: const EdgeInsets.all(8),
+        children: todos.map((todo) => TodoItem(todo: todo)).toList())
+        : const Center(
+      child: Text('Tap button below to add a todo!'),
+    );
+  }
+}
+
+class TodoItem extends StatelessWidget {
+  const TodoItem({
+    required this.todo,
+    Key? key,
+  }) : super(key: key);
+
+  final double iconSize = 24.0;
+  final Todo todo;
+
+  void _deleteTodo(BuildContext context) async {
+    try {
+      // to delete data from DataStore, you pass the model instance to
+      // Amplify.DataStore.delete()
+      await Amplify.DataStore.delete(todo);
+    } catch (e) {
+      safePrint('An error occurred while deleting Todo: $e');
+    }
+  }
+
+  Future<void> _toggleIsComplete() async {
+
+    // copy the Todo you wish to update, but with updated properties
+    final updatedTodo = todo.copyWith(isComplete: !todo.isComplete);
+    try {
+
+      // to update data in DataStore, you again pass an instance of a model to
+      // Amplify.DataStore.save()
+      await Amplify.DataStore.save(updatedTodo);
+    } catch (e) {
+      safePrint('An error occurred while saving Todo: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: InkWell(
+        onTap: () {
+          _toggleIsComplete();
+        },
+        onLongPress: () {
+          _deleteTodo(context);
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    todo.name,
+                    style: const TextStyle(
+                        fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  Text(todo.description ?? 'No description'),
+                ],
+              ),
+            ),
+            Icon(
+                todo.isComplete
+                    ? Icons.check_box
+                    : Icons.check_box_outline_blank,
+                size: iconSize),
+          ]),
+        ),
+      ),
+    );
+  }
+}
+
+class AddTodoForm extends StatefulWidget {
+  const AddTodoForm({Key? key}) : super(key: key);
+
+  @override
+  State<AddTodoForm> createState() => _AddTodoFormState();
+}
+
+class _AddTodoFormState extends State<AddTodoForm> {
+  late final TextEditingController _nameController;
+  late final TextEditingController _descriptionController;
+
+  @override
+  void initState() {
+    _nameController = TextEditingController();
+    _descriptionController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _nameController.dispose();
+    _descriptionController.dispose();
+  }
+
+  Future<void> _saveTodo() async {
+    // get the current text field contents
+    final name = _nameController.text;
+    final description = _descriptionController.text;
+    // create a new Todo from the form values
+    // `isComplete` is also required, but should start false in a new Todo
+    final newTodo = Todo(
+      name: name,
+      description: description.isNotEmpty ? description : null,
+      isComplete: false,
+    );
+    try {
+      // to write data to DataStore, you simply pass an instance of a model to
+      // Amplify.DataStore.save()
+      await Amplify.DataStore.save(newTodo);
+      // after creating a new Todo, close the form
+      // Be sure the context at that moment is still valid and mounted
+      if (mounted) {
+        Navigator.of(context).pop();
+      }
+    } catch (e) {
+      safePrint('An error occurred while saving Todo: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add Todo'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextFormField(
+                controller: _nameController,
+                decoration:
+                const InputDecoration(filled: true, labelText: 'Name'),
+              ),
+              TextFormField(
+                controller: _descriptionController,
+                decoration: const InputDecoration(
+                    filled: true, labelText: 'Description'),
+              ),
+              ElevatedButton(
+                onPressed: _saveTodo,
+                child: const Text('Save'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
